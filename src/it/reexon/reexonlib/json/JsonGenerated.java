52ea0dfb141ee96eb1fe523c83931ede3a77de36
marco.velluto <marco.velluto@csttech.it>
2016-02-08 11:50:16 +0100

    add JsonGenerated

---
+++ src/it/reexon/reexonlib/json/JsonGenerated.java
@@ -0,0 +1,38 @@
+package it.reexon.reexonlib.json;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import it.reexon.reexonlib.json.dtos.MapRequestDTO;
+
+
+public class JsonGenerated
+{
+    public static void main(String[] args)
+    {
+        MapRequestDTO mapRequestDTO = new MapRequestDTO();
+        Gson gson = new Gson();
+
+        // convert java object to JSON format,
+        // and returned as JSON formatted string
+        String json = gson.toJson(mapRequestDTO);
+
+        try
+        {
+            //write converted json data to a file named "file.json"
+            FileWriter writer = new FileWriter("c:\\temp\\file.json");
+            writer.write(json);
+            writer.close();
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println(json);
+
+    }
+}

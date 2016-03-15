96d6fe2a6180891cfb8bb52025766b3e737fccd2
marco.velluto <marco.velluto@csttech.it>
2016-03-15 17:49:21 +0100

    Add new Crypt algorithms

---
+++ src/it/reexon/reexon/lib/security/algorithms/CipherAlgorithms.java
@@ -0,0 +1,15 @@
+package it.reexon.reexon.lib.security.algorithms;
+
+public class CipherAlgorithms
+{
+    private CipherAlgorithms() {
+        // cannot be instantiated.
+    }
+    
+    /**
+     * The MD2 message digest algorithm defined in RFC 1319.
+     */
+    public static final String AES = "AES";
+
+    
+}

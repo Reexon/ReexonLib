9f865b3c5d42e8dcba1cf99fd2374e6e1506c378
marco.velluto <marco.velluto@csttech.it>
2016-03-30 17:51:53 +0200

    add new classes to run command ping

---
+++ src/it/reexon/reexon/lib/system/commands/ping/JPingy.java
@@ -0,0 +1,192 @@
+/**
+ Copyright (c) 2016 Marco Velluto
+ */
+package it.reexon.reexon.lib.system.commands.ping;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import it.reexon.reexon.lib.system.commands.ping.enums.SystemEnum;
+
+
+public class JPingy
+{
+
+    public static final int SECOND_ONE_DAY = 86400;
+
+    private Boolean realtimeWrite = true;
+    private String fileName;
+    private String location = "C:\\TEMP\\";
+    private Integer count;
+    private String url;
+    private SystemEnum system;
+
+    /**
+     * 
+     * @param fileName
+     * @param url
+     * @param count
+     * @param system    Opertive system. If null set WINDOWS
+     */
+    public JPingy(String fileName, String url, int count, SystemEnum system)
+    {
+        super();
+        this.fileName = fileName;
+        this.count = count;
+        this.url = url;
+        this.system = system != null ? system : SystemEnum.WINDOWS;
+    }
+
+    public void run()
+    {
+        BufferedReader reader = null;
+        Process process = null;
+        File file = new File(fileName);
+        PingArguments pingArguments = new PingArguments.Builder(this.system).build().setUrl(url);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true)))
+        {
+            writer.write(createMessage("BEGIN PROCESS", new Date()));
+            writer.newLine();
+
+            pingArguments.setCount(this.count);
+
+            Runtime runtime = Runtime.getRuntime();
+            process = runtime.exec(pingArguments.getCommand());
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            write(writer, createMessage(pingArguments.getCommand(), new Date()));
+
+            String inputLine;
+            List<String> linesResult = new ArrayList<String>();
+            while ((inputLine = reader.readLine()) != null)
+            {
+                Date date = new Date();
+                String message = "";
+                writer.newLine();
+                message = createMessage(inputLine, date);
+
+                System.out.println(message);
+
+                write(writer, message);
+                linesResult.add(inputLine);
+            }
+            writer.newLine();
+            writer.write(createMessage("END PROCESS", new Date()));
+            analyzeResult(linesResult);
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
+        finally
+        {
+            closeReader(reader);
+            if (process != null)
+                process.destroy();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private void analyzeResult(List<String> linesResult)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void closeReader(BufferedReader reader)
+    {
+        if (reader != null)
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void write(BufferedWriter writer, String message) throws IOException
+    {
+        writer.write(message);
+        if (realtimeWrite)
+            writer.flush();
+    }
+
+    private static String createMessage(String inputLine, Date date)
+    {
+        StringBuilder message = new StringBuilder();
+        message.append(date + " ");
+        message.append(inputLine);
+
+        return message.toString();
+    }
+
+    public Boolean getRealtimeWrite()
+    {
+        return realtimeWrite;
+    }
+
+    public void setRealtimeWrite(Boolean realtimeWrite)
+    {
+        this.realtimeWrite = realtimeWrite;
+    }
+
+    public String getFileName()
+    {
+        return fileName;
+    }
+
+    public void setFileName(String fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+
+    public Integer getCount()
+    {
+        return count;
+    }
+
+    public void setCount(Integer count)
+    {
+        this.count = count;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public void setUrl(String url)
+    {
+        this.url = url;
+    }
+
+    public SystemEnum getSystem()
+    {
+        return system;
+    }
+
+    public void setSystem(SystemEnum system)
+    {
+        this.system = system;
+    }
+}
---
+++ src/it/reexon/reexon/lib/system/commands/ping/PingArguments.java
@@ -0,0 +1,235 @@
+/** Copyright (c) 2016 Marco Velluto
+ * 
+ */
+package it.reexon.reexon.lib.system.commands.ping;
+
+import org.apache.commons.lang3.StringUtils;
+
+import it.reexon.reexon.lib.system.commands.ping.enums.SystemEnum;
+
+
+/**
+ * @author marco.velluto
+ */
+public class PingArguments
+{
+    private String url;
+    private Integer count;
+    private Long timeout;
+    private Integer payload_bytes;
+    private Integer interval;
+    private Integer ttl;
+    private SystemEnum system;
+
+    public static class Builder
+    {
+
+        private PingArguments arguments;
+
+        public Builder(SystemEnum systemEnum)
+        {
+            this.arguments = new PingArguments();
+            this.arguments.setSystem(systemEnum);
+        }
+
+        public Builder setUrl(String url)
+        {
+            this.arguments.url = url;
+            return this;
+        }
+
+        public Builder setCount(int count)
+        {
+            this.arguments.count = count;
+            return this;
+        }
+
+        public Builder setTimeout(long timeout)
+        {
+            this.arguments.timeout = timeout;
+            return this;
+        }
+
+        public Builder setBytes(int bytes)
+        {
+            this.arguments.payload_bytes = bytes;
+            return this;
+        }
+
+        public Builder setTtl(int ttl)
+        {
+            this.arguments.ttl = ttl;
+            return this;
+        }
+
+        /**
+         * -i
+         * 
+         * @param interval
+         * @return
+         */
+        public Builder setInterval(Integer interval)
+        {
+            this.arguments.interval = interval;
+            return this;
+        }
+
+        public PingArguments build()
+        {
+            return arguments;
+        }
+    }
+
+    /**
+     * -c       Send N packets specified with -c
+     * @return
+     */
+    //Unix Systems
+    public String getCommand()
+    {
+        if (StringUtils.isBlank(this.url))
+            throw new IllegalArgumentException("URL is mandatory");
+
+        if (this.system == SystemEnum.UNIX)
+        {
+            return this.unixCommand();
+        }
+        else if (this.system == SystemEnum.WINDOWS)
+        {
+            return this.windowsCommand();
+        }
+        else
+        {
+            throw new RuntimeException("Not foud command to system: " + this.system);
+        }
+    }
+
+    /**
+     * -n Count         Determines the number of echo requests to send. The default is 4 requests.
+     * -w Timeout       Enables you to adjust the time-out (in milliseconds). The default is 1,000 (a 1-second time-out).
+     * -l Size          Enables you to adjust the size of the ping packet. The default size is 32 bytes.
+     * -f               Sets the Do Not Fragment bit on the ping packet. By default, the ping packet allows fragmentation.
+     * 
+     * @see https://technet.microsoft.com/en-us/library/cc737478(v=ws.10).aspx
+     * @return
+     */
+    private String windowsCommand()
+    {
+        return this.createCommand("-n", "-w", "-l");
+    }
+
+    /**
+     * @see http://www.thegeekstuff.com/2009/11/ping-tutorial-13-effective-ping-command-examples/
+     * @return
+     */
+    private String unixCommand()
+    {
+        return createCommand("-c", "-W", "-s");
+    }
+
+    /**
+     * 
+     * @param coutn 
+     *          * (Windows: -c) 
+     *          * (Unix: -n)
+     *          
+     * @param timeout 
+     *                  * (Windows: -w) 
+     *                  * (Unix: -W)
+     *                  
+     * @param payload_bytes
+     *                  * (Windows: -l)
+     *                  * (Unix: -s)
+     * @return
+     */
+    private String createCommand(String count, String timeout, String payload_bytes)
+    {
+        if (StringUtils.isBlank(this.url))
+            throw new IllegalArgumentException("URL is mandatory");
+
+        StringBuilder command = new StringBuilder();
+
+        command.append("ping").append(" ");
+        if (this.count != null)
+            command.append(count).append(" ").append(this.count).append(" ");
+        if (this.timeout != null)
+            command.append(timeout).append(" ").append(this.timeout).append(" ");
+        if (this.payload_bytes != null)
+            command.append(payload_bytes).append(" ").append(this.payload_bytes).append(" ");
+        command.append(url);
+
+        return command.toString();
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public PingArguments setUrl(String url)
+    {
+        this.url = url;
+        return this;
+    }
+
+    public Integer getCount()
+    {
+        return count;
+    }
+
+    public void setCount(Integer count)
+    {
+        this.count = count;
+    }
+
+    public Long getTimeout()
+    {
+        return timeout;
+    }
+
+    public void setTimeout(Long timeout)
+    {
+        this.timeout = timeout;
+    }
+
+    public Integer getPayload_bytes()
+    {
+        return payload_bytes;
+    }
+
+    public void setPayload_bytes(Integer payload_bytes)
+    {
+        this.payload_bytes = payload_bytes;
+    }
+
+    public SystemEnum getSystem()
+    {
+        return system;
+    }
+
+    public void setSystem(SystemEnum system)
+    {
+        this.system = system;
+    }
+
+    public Integer getInterval()
+    {
+        return interval;
+    }
+
+    public void setInterval(Integer interval)
+    {
+        this.interval = interval;
+    }
+
+    public Integer getTtl()
+    {
+        return ttl;
+    }
+
+    public void setTtl(Integer ttl)
+    {
+        this.ttl = ttl;
+    }
+
+}

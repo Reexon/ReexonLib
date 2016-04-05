f46e42e36e1710a9e52ea9826ca080b56b0d3952
marco.velluto <marco.velluto@csttech.it>
2016-04-05 18:07:00 +0200

    Add class and methods to extract images form pdf

---
+++ src/it/reexon/reexon/lib/exceptions/NotFoundException.java
@@ -0,0 +1,43 @@
+package it.reexon.reexon.lib.exceptions;
+
+public class NotFoundException extends Exception
+{
+    private static final long serialVersionUID = 1L;
+
+    private Throwable throwable;
+    private String message;
+
+    public NotFoundException(String message)
+    {
+        super();
+        this.message = message;
+    }
+
+    public NotFoundException(Throwable throwable, String message)
+    {
+        super();
+        this.throwable = throwable;
+        this.message = message;
+    }
+
+    public Throwable getThrowable()
+    {
+        return throwable;
+    }
+
+    public void setThrowable(Throwable throwable)
+    {
+        this.throwable = throwable;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+
+}
---
+++ src/it/reexon/reexon/lib/pdf/PDFUtils.java
@@ -0,0 +1,69 @@
+package it.reexon.reexon.lib.pdf;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+import it.reexon.reexon.lib.list.ListUtil;
+
+
+/**
+ * 
+ * @author Marco Velluto
+ * @since Java 1.8
+ */
+public class PDFUtils
+{
+    /**
+     * 
+     * @param sourcePDF pdf to fetch images
+     * @param destinationFolder destination folder
+     * 
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    public static void createImages(File sourcePDF, File destinationFolder) throws IOException, FileNotFoundException
+    {
+        if (!destinationFolder.exists())
+        {
+            destinationFolder.mkdir();
+            System.out.println("Folder Created -> " + destinationFolder.getAbsolutePath());
+        }
+        if (sourcePDF.exists())
+        {
+            try (PDDocument document = PDDocument.load(sourcePDF);)
+            {
+                System.out.println("Images copied to Folder: " + destinationFolder.getName());
+
+                List<?> list = document.getDocumentCatalog().getAllPages();
+                List<PDPage> listPage = ListUtil.castList(PDPage.class, list);
+
+                System.out.println("Total files to be converted -> " + list.size());
+
+                String fileName = sourcePDF.getName().replace(".pdf", "");
+                int pageNumber = 1;
+                for (PDPage page : listPage)
+                {
+                    BufferedImage image = page.convertToImage();
+                    File outputfile = new File(destinationFolder + "\\" + fileName + "_" + pageNumber + ".png");
+                    System.out.println("Image Created -> " + outputfile.getName());
+                    ImageIO.write(image, "png", outputfile);
+                    pageNumber++;
+                }
+                System.out.println("Converted Images are saved at -> " + destinationFolder.getAbsolutePath());
+            }
+        }
+        else
+        {
+            System.err.println(sourcePDF.getName() + " File not exists");
+            throw new FileNotFoundException(sourcePDF.getName() + " File not exists");
+        }
+    }
+}

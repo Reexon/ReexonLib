90641249b7958a75bad7e09341e63991bfc9714b
marco.velluto <marco.velluto@csttech.it>
2016-04-08 15:49:18 +0200

    Add images Tests

---
+++ src/it/reexon/reexon/lib/images/tests/ImageUtilsTest.java
@@ -0,0 +1,24 @@
+package it.reexon.reexon.lib.images.tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+public class ImageUtilsTest
+{
+
+    @Test
+    public final void test()
+    {
+        try
+        {
+            fail("TO IMPLEMENT");
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
+        }
+    }
+
+}

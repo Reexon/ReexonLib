2acb1001d70f1009f327011cafd9bf0bf28b3e25
Marco Velluto <marco.velluto@csttech.it>
2016-05-10 09:40:03 +0200

    Add ImageUtilsTest

---
+++ test/it/reexon/lib/images/tests/ImageUtilsTest.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2016 Marco Velluto. All rights reserved.
+ */
+package it.reexon.lib.images.tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ * @author Marco Velluto
+ * @since Java 1.8
+ */
+public class ImageUtilsTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {}
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {}
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {}
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {}
+
+    /**
+     * Test method for {@link it.reexon.lib.images.ImageUtils#convertFormat(java.lang.String, java.lang.String, it.reexon.lib.images.ConvertType)}.
+     */
+    @Test
+    public final void testConvertFormat()
+    {
+        // TODO
+    }
+
+    /**
+     * Test method for {@link it.reexon.lib.images.ImageUtils#imageToBitmap(java.awt.image.BufferedImage, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testImageToBitmap()
+    {
+        // TODO
+    }
+
+    /**
+     * Test method for {@link it.reexon.lib.images.ImageUtils#scaleImageTo256kb(java.nio.file.Path)}.
+     */
+    @Test
+    public final void testScaleImageTo256kb()
+    {
+        // TODO
+    }
+
+    /**
+     * Test method for {@link it.reexon.lib.images.ImageUtils#canEncodeImage(javax.imageio.ImageWriter, javax.imageio.ImageTypeSpecifier)}.
+     */
+    @Test
+    public final void testCanEncodeImage()
+    {
+        // TODO
+    }
+
+    /**
+     * Test method for {@link it.reexon.lib.images.ImageUtils#addWatermark(java.lang.String)}.
+     */
+    @Test
+    public final void testAddWatermarkString()
+    {
+        // TODO
+    }
+
+    /**
+     * Test method for {@link it.reexon.lib.images.ImageUtils#addWatermark(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testAddWatermarkStringString()
+    {
+        // TODO
+    }
+
+}

